24b72d5efc016dca31ae71acba5273f5cb1ef2b1
Fernando De Mennato <fdemennato@progesoftware.it>
2020-12-29 09:55:56 +0100

    add jest

--- test/specs/vue-numeric.spec.js
+++ test/specs/vue-numeric.spec.js
@@ -7,54 +7,54 @@ import VueNumeric from '../../src/vue-numeric';
 describe('vue-numeric.vue', () => {
   it('Use default decimal separator', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000 }})
-    expect(wrapper.data().amount).to.equal('2,000')
+    expect(wrapper.data().amount).toBe('2,000');
   })
 
   it('format values with currency prefix', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000, currency: 'USD' }})
-    expect(wrapper.data().amount).to.equal('USD 2,000')
+    expect(wrapper.data().amount).toBe('USD 2,000');
   })
 
   it('format values with currency suffix', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000, currency: 'CZK', currencySymbolPosition: 'suffix' }})
-    expect(wrapper.data().amount).to.equal('2,000 CZK')
+    expect(wrapper.data().amount).toBe('2,000 CZK');
   })
 
   it('format values with decimals', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000.34, precision: 2, currency: '$' }})
-    expect(wrapper.data().amount).to.equal('$ 2,000.34')
+    expect(wrapper.data().amount).toBe('$ 2,000.34');
   })
 
   it('format values with decimals even when no decimal specified', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000, precision: 2, currency: '$' }})
-    expect(wrapper.data().amount).to.equal('$ 2,000.00')
+    expect(wrapper.data().amount).toBe('$ 2,000.00');
   })
 
   it('format values with decimals rounded', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000.36, precision: 1, currency: '$' }})
-    expect(wrapper.data().amount).to.equal('$ 2,000.4')
+    expect(wrapper.data().amount).toBe('$ 2,000.4');
   })
 
   it('format values with . separator', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000000, currency: '$', separator: '.', precision: 2 }})
-    expect(wrapper.data().amount).to.equal('$ 2.000.000,00')
+    expect(wrapper.data().amount).toBe('$ 2.000.000,00');
   })
 
   it('format values with , separator', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000000, currency: '$', separator: ',', precision: 2 }})
-    expect(wrapper.data().amount).to.equal('$ 2,000,000.00')
+    expect(wrapper.data().amount).toBe('$ 2,000,000.00');
   })
 
   it('format values with space separator', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000000, currency: '$', separator: 'space', precision: 2  }})
-    expect(wrapper.data().amount).to.equal('$ 2 000 000,00')
+    expect(wrapper.data().amount).toBe('$ 2 000 000,00');
   })
 
   it(
     'format values with correct decimals symbol when using different thousand separator',
     () => {
       const wrapper = mount(VueNumeric, { propsData: { modelValue: 20000.36, precision: 1, currency: '$', separator: '.' }})
-      expect(wrapper.data().amount).to.equal('$ 20.000,4')
+      expect(wrapper.data().amount).toBe('$ 20.000,4');
     }
   )
 
@@ -66,7 +66,7 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(typeof wrapper.data().total).to.equal('number')
+    expect(typeof wrapper.data().total).toBe('number');
   })
 
   it('outputs String if specified', () => {
@@ -77,15 +77,15 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(typeof wrapper.data().total).to.equal('string')
+    expect(typeof wrapper.data().total).toBe('string');
   })
 
   it('is <span> tag in read-only mode', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000, currency: '$', readOnly: true, readOnlyClass: 'test-class' }})
     wrapper.update()
-    expect(wrapper.is('span')).to.equal(true)
-    expect(wrapper.hasClass('test-class')).to.equal(true)
-    expect(wrapper.text()).is.equal('$ 2,000')
+    expect(wrapper.is('span')).toBe(true);
+    expect(wrapper.hasClass('test-class')).toBe(true);
+    expect(wrapper.text()).toBe('$ 2,000');
   })
 
   it('apply class when read-only mode enabled', done => {
@@ -95,7 +95,7 @@ describe('vue-numeric.vue', () => {
     wrapper.setProps({ readOnly: true })
 
     wrapper.instance().$nextTick(() => {
-      expect(wrapper.instance().$el.className).to.equal('testclass')
+      expect(wrapper.instance().$el.className).toBe('testclass');
       done()
     })
   })
@@ -106,7 +106,7 @@ describe('vue-numeric.vue', () => {
 
     wrapper.setProps({ readOnly: false })
     wrapper.instance().$nextTick(() => {
-      expect(wrapper.instance().$el.className).to.equal('')
+      expect(wrapper.instance().$el.className).toBe('');
       done()
     })
   })
@@ -119,7 +119,7 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(wrapper.data().total).to.equal(100)
+    expect(wrapper.data().total).toBe(100);
   })
 
   it('cannot below min props', () => {
@@ -130,7 +130,7 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(wrapper.data().total).to.equal(200)
+    expect(wrapper.data().total).toBe(200);
   })
 
   it('process valid value ', () => {
@@ -141,7 +141,7 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(wrapper.data().total).to.equal(100)
+    expect(wrapper.data().total).toBe(100);
   })
 
   it('allow minus value when minus props is true', () => {
@@ -152,7 +152,7 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(wrapper.data().total).to.equal(-150)
+    expect(wrapper.data().total).toBe(-150);
   })
 
   it('disallow minus value when minus props is false', () => {
@@ -163,7 +163,7 @@ describe('vue-numeric.vue', () => {
     })
 
     const wrapper = mount(component)
-    expect(wrapper.data().total).to.equal(0)
+    expect(wrapper.data().total).toBe(0);
   })
 
   it('updates delayed value with format if without focus', done => {
@@ -180,63 +180,63 @@ describe('vue-numeric.vue', () => {
     }, 100);
 
     setTimeout(() => {
-      expect(vm.$el.firstChild.value.trim()).to.equal('3,000')
+      expect(vm.$el.firstChild.value.trim()).toBe('3,000');
       done()
     }, 500);
   })
 
   it('remove space if currency props undefined', () => {
     const wrapper = mount(VueNumeric, {propsData: { modelValue: 2000 }})
-    expect(wrapper.data().amount).to.equal('2,000')
+    expect(wrapper.data().amount).toBe('2,000');
   })
 
   it('format value on blur', () => {
     const wrapper = mount(VueNumeric, {propsData: { modelValue: 2000 }})
     wrapper.trigger('blur')
-    expect(wrapper.data().amount).to.equal('2,000')
+    expect(wrapper.data().amount).toBe('2,000');
   })
 
   it('clear the field if zero value', () => {
     const wrapper = mount(VueNumeric, {propsData: { modelValue: 0, separator: '.', precision: 2 }})
     wrapper.trigger('focus')
-    expect(wrapper.data().amount).to.equal(null)
+    expect(wrapper.data().amount).toBe(null);
   })
 
   it('remove thousand separator and symbol on focus with , decimal', () => {
     const wrapper = mount(VueNumeric, {propsData: { modelValue: 2000.21, separator: '.', precision: 2 }})
     wrapper.trigger('focus')
-    expect(wrapper.data().amount).to.equal('2000,21')
+    expect(wrapper.data().amount).toBe('2000,21');
   })
 
   it('remove thousand separator and symbol on focus with . decimal', () => {
     const wrapper = mount(VueNumeric, {propsData: { modelValue: 2000.21, separator: ',', precision: 2 }})
     wrapper.trigger('focus')
-    expect(wrapper.data().amount).to.equal('2000.21')
+    expect(wrapper.data().amount).toBe('2000.21');
   })
 
   it('process value on input', () => {
     const process = jest.fn();
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000 }, methods: { process }})
     wrapper.trigger('input')
-    expect(process.called).to.equal(true)
+    expect(process.called).toBe(true);
   })
 
   it('does not show default value when placeholder if defined', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000, placeholder: 'number here' }})
-    expect(wrapper.data().amount).to.equal('')
+    expect(wrapper.data().amount).toBe('');
   })
 
   it(
     'sets the value to 0 when no empty value is provided and input is empty',
     () => {
       const wrapper = mount(VueNumeric, { propsData: { modelValue: '' }})
-      expect(wrapper.data().amount).to.equal('0')
+      expect(wrapper.data().amount).toBe('0');
     }
   )
 
   it('uses the provided empty value when input is empty', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: '', emptyValue: 1 }})
-    expect(wrapper.data().amount).to.equal('1')
+    expect(wrapper.data().amount).toBe('1');
   })
 
   it(
@@ -249,7 +249,7 @@ describe('vue-numeric.vue', () => {
       })
 
       const wrapper = mount(component)
-      expect(wrapper.data().total).to.equal(150)
+      expect(wrapper.data().total).toBe(150);
     }
   )
 
@@ -263,26 +263,26 @@ describe('vue-numeric.vue', () => {
       })
 
       const wrapper = mount(component)
-      expect(wrapper.data().total).to.equal(0)
+      expect(wrapper.data().total).toBe(0);
     }
   )
 
   it('apply new separator immediately if it is changed', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000, separator: ',' }})
     wrapper.setProps({ separator: '.' })
-    expect(wrapper.data().amount).to.equal('2.000')
+    expect(wrapper.data().amount).toBe('2.000');
   })
 
   it('apply new currency prop immediately if it is changed', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 0, currency: '$' }})
     wrapper.setProps({ currency: 'USD' })
-    expect(wrapper.data().amount).to.equal('USD 0')
+    expect(wrapper.data().amount).toBe('USD 0');
   })
 
   it('apply new precision immediately if it is changed', () => {
     const wrapper = mount(VueNumeric, { propsData: { modelValue: 2000.17, precision: 2 }})
     wrapper.setProps({ precision: 1 })
-    expect(wrapper.data().amount).to.equal('2,000.2')
+    expect(wrapper.data().amount).toBe('2,000.2');
   })
 
   it('allow to use arbitrary separators', () => {
@@ -294,6 +294,6 @@ describe('vue-numeric.vue', () => {
         decimalSeparator: ','
       }
     })
-    expect(wrapper.data().amount).to.equal('1 000,94')
+    expect(wrapper.data().amount).toBe('1 000,94');
   })
 })
